b6ad1916b4b894a208865dfad1c0690febf78e39
Tomas Solian <tomassolian03@gmail.com>
2019-09-18 20:08:59 -0300

    Initial commit

---
+++ app/src/main/java/com/example/actividad43/ui/send/SendFragment.java
@@ -0,0 +1,34 @@
+package com.example.actividad43.ui.send;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
+
+import com.example.actividad43.R;
+
+public class SendFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_send, container, false);
+        Button enviar = root.findViewById(R.id.enviar);
+        enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(getActivity(), R.id.nav_host_fragment).navigate(R.id.enviado);
+            }
+        });
+        return root;
+    }
+}
